a2e41f6a615cf42f6924a6bb3315dee0eec31063
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2025-07-09 22:12:18 +0200

    added some bits and bobs

--- src/routes/day/[date]/+page.svelte
+++ src/routes/day/[date]/+page.svelte
@@ -1,1 +1,29 @@
-<div><p>This is the day page</p></div>
+<div>
+    <div>
+        <button onclick={() => go_back()}>&lt</button>
+        <p>{day}</p>
+        <button onclick={() => go_forward()}>&gt</button>
+    </div>
+    <div>
+        <p>Home 6/10</p>
+        <p>Work 6/10</p>
+    </div>
+    <button>pick location</button>
+</div>
+
+<script lang="ts">
+
+    import { goto } from "$app/navigation";
+    import { page } from '$app/state'; 
+
+    $: day = page.params.date;
+
+
+    function go_back() {
+        goto("/day/2025-01-01")
+        
+    }
+    function go_forward() {
+        
+    }
+</script>
