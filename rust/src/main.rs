d4e5db7b1dfa96935d363dbc944043a9ca0d2143
gvdbroek <141612818+gvdbroek@users.noreply.github.com>
2025-12-19 20:49:44 +0100

    start imple

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,29 @@
+use std::collections::HashMap;
+
 use color_eyre::Result;
 use crossterm::event::{self, Event};
 use ratatui::{DefaultTerminal, Frame};
+
+/// Context of app
+pub struct Context {
+    pub panels: HashMap<String, Box<dyn Panel>>,
+    pub focussed: String,
+}
+
+pub trait Panel {
+    fn render(&self, frame: &mut Frame, area: ratatui::layout::Rect);
+    fn handle_input(&mut self, event: Event);
+    fn is_focussed(&self, event: Event) -> bool;
+}
+
+pub struct DebugPanel {}
+impl Panel for DebugPanel {
+    fn render(&self, frame: &mut Frame, area: ratatui::layout::Rect) {}
+    fn handle_input(&mut self, event: Event) {}
+    fn is_focussed(&self, event: Event) -> bool {
+        true
+    }
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -11,8 +34,16 @@ fn main() -> Result<()> {
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    // init context and panels
+    let mut context = Context {
+        panels: HashMap::new(),
+        focussed: "main".into(),
+    };
+    context.panels["main".into()] = DebugPanel {};
+
     loop {
         terminal.draw(render)?;
+
         if let Event::Key(key) = event::read()? {
             match key.code {
                 event::KeyCode::Char(c) => {
